e5026c74e19bb901ecabce04033a08a3b70bdaed
Yana Peshko <yanapeshko917@gmail.com>
2023-06-29 19:37:51 -0400

    organized structure+installed canvas

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -1,3 +1,5 @@
+const draw = require("../common/draw.js");
+
 const constants = {};
 
 constants.DATA_DIR = "../data";
@@ -9,4 +11,36 @@ constants.SAMPLES = constants.DATASET_DIR + "/samples.json";
 
 const fs = require("fs");
 
-const fileName = fs.readdirSync(constants.RAW_DIR);
+const fileNames = fs.readdirSync(constants.RAW_DIR);
+const samples = [];
+let id = 1;
+fileNames.forEach((fn) => {
+  const content = fs.readFileSync(constants.RAW_DIR + "/" + fn);
+  const { session, student, drawings } = JSON.parse(content);
+  for (let label in drawings) {
+    samples.push({
+      id,
+      label,
+      student_name: student,
+      student_id: session,
+    });
+
+    const paths = drawings[label];
+    fs.writeFileSync(
+      constants.JSON_DIR + "/" + id + ".json",
+      JSON.stringify(drawings[paths])
+    );
+
+    generateImageFile(constants.IMG_DIR + "/" + id + ".png", paths);
+    id++;
+  }
+});
+
+fs.writeFileSync(constants.SAMPLES, JSON.stringify(samples));
+
+function generateImageFile(outFile, paths) {
+  draw.paths(ctx, paths);
+
+  const buffer = canvas.toBuffer("image/png");
+  fs.writeFileSync(outFile, buffer);
+}
