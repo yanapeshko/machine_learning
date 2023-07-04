269defc3be8b819a1ac8bf89f1a4a913c1ed1101
Yana Peshko <yanapeshko917@gmail.com>
2023-07-04 10:06:05 -0400

    added google chart

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -3,6 +3,8 @@ const features = require("../common/features.js");
 
 const fs = require("fs");
 
+console.log("EXTRACTING FEATURES...");
+
 const samples = JSON.parse(fs.readFileSync(constants.SAMPLES));
 
 for (const sample of samples) {
@@ -19,6 +21,17 @@ fs.writeFileSync(
   constants.FEATURES,
   JSON.stringify({
     featureNames,
-    samples,
+    samples: samples.map((s) => {
+      return {
+        point: s.point,
+        label: s.label,
+      };
+    }),
   })
 );
+fs.writeFileSync(
+  constants.FEATURES_JS,
+  `const features=${JSON.stringify({ featureNames, samples })}`
+);
+
+console.log("DONE!");
