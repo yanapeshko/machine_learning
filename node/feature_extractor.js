7fd2d3614336e70cd2a81e9000da6eda47d0fb75
Yana Peshko <yanapeshko917@gmail.com>
2023-07-03 21:49:26 -0400

    added feature

---
+++ node/feature_extractor.js
@@ -0,0 +1,24 @@
+const constants = require("../common/constants.js");
+const features = require("../common/features.js");
+
+const fs = require("fs");
+
+const samples = JSON.parse(fs.readFileSync(constants.SAMPLES));
+
+for (const sample of samples) {
+  const paths = JSON.parse(
+    fs.readFileSync(constants.JSON_DIR + "/" + sample.id + ".json")
+  );
+
+  sample.point = [features.getPathCount(paths), features.getPointCount(paths)];
+}
+
+const featureNames = ["Path Count", "Point Count"];
+
+fs.writeFileSync(
+  constants.FEATURES,
+  JSON.stringify({
+    featureNames,
+    samples,
+  })
+);
