904205fc2b62fd3c34c253daad93ccfd1bf2592d
Yana Peshko <yanapeshko917@gmail.com>
2023-07-06 18:11:57 -0400

    done with full chart functionality

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 In this No Black Box Machine Learning Course in JavaScript, you will gain a deep understanding of machine learning systems by coding without relying on libraries. This unique approach not only demystifies the inner workings of machine learning but also significantly enhances software development skills.
-1:33:59/2:00:00😁
+2:00:00/2:01:37😁
--- web/js/display.js
+++ web/js/display.js
@@ -13,6 +13,7 @@ function createRow(container, studentName, samples) {
 
     const sampleContainer = document.createElement("div");
     sampleContainer.id = "sample_" + id;
+    sampleContainer.onclick = () => handleClick(sample, false);
     sampleContainer.classList.add("sampleContainer");
 
     const sampleLabel = document.createElement("div");
@@ -31,7 +32,28 @@ function createRow(container, studentName, samples) {
   }
 }
 
-function handleClick(sample) {
+function handleClick(sample, doScroll = true) {
+  if (sample == null) {
+    [...document.querySelectorAll(".emphasize")].forEach((e) =>
+      e.classList.remove("emphasize")
+    );
+    return;
+  }
   const el = document.getElementById("sample_" + sample.id);
-  el.classList.add("emphasize")
+  if (el.classList.contains("emphasize")) {
+    el.classList.remove("emphasize");
+    chart.selectSample(null);
+    return;
+  }
+  [...document.querySelectorAll(".emphasize")].forEach((e) =>
+    e.classList.remove("emphasize")
+  );
+  el.classList.add("emphasize");
+  if (doScroll) {
+    el.scrollIntoView({
+      behavior: "auto",
+      block: "center",
+    });
+  }
+  chart.selectSample(sample);
 }
